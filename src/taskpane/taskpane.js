aa67382aaaf18575570a3dce0cd5c62d4f0115da
INOPIAE <m.maengel@inopiae.de>
2022-01-26 07:24:57 +0100

    add: implement number of digits

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -14,6 +14,19 @@ export async function iround() {
     await Excel.run(async (context) => {
       let myRng = context.workbook.getSelectedRange();
       myRng.load(["values", "text", "formulas", "formulasR1C1"]);
+/*      var checkBoxNum = document.getElementById("inochknum");
+      console.log(checkBoxNum);
+      console.log(checkBoxNum.checked);*/
+
+      // define digits
+      var digits = document.getElementById("inoDigits");
+      var digit = 2;
+      //ToDo check digit isNumeric
+      if (digits.value.indexOf(".") < 0) {
+        digit = -digits.value.length + 1;
+      } else {
+        digit = digits.value.length - 2;
+      }
 
       await context.sync();
       let myFormulas = myRng.formulasR1C1;
@@ -32,8 +45,8 @@ export async function iround() {
           let test = colString.substring(0, 1);
 
           if (test == "=") {
-            let newFormula = colString.replace("=", "=ROUND(") + ", 2)";
-            //console.log(newFormula);
+            let newFormula = colString.replace("=", "=ROUND(") + ", " + digit + ")";
+            console.log(newFormula);
             if (colString.length >= 6) {
               let test = colString.substring(0, 6);
               if (test == "=ROUND") {
@@ -44,6 +57,12 @@ export async function iround() {
             } else {
               myInnerArray.push(newFormula);
             }
+/*          } else if (checkBoxNum.checked == true) {
+            if (isNaN(col) == true) {
+              let newFormula = "=ROUND(" + colString + ", 2)";
+              console.log(newFormula);
+              //myInnerArray.push(newFormula);
+            }*/
           } else {
             myInnerArray.push(col);
           }
@@ -79,15 +98,14 @@ export async function iunround() {
 
         // then loop through each column
         row.forEach(function (col) {
-          console.log(col);
           let colString = col.toString();
           let test = colString.substring(0, 6);
 
           if (test == "=ROUND") {
-            let newFormula = colString.replace("=ROUND(", "=");
-            newFormula = newFormula.replace(", 2)", "");
-            console.log(newFormula);
-
+            let newFormula = colString.replace("=ROUNDDOWN(", "=");
+            newFormula = newFormula.replace("=ROUNDUP(", "=");
+            newFormula = newFormula.replace("=ROUND(", "=");
+            newFormula = newFormula.substring(0, newFormula.lastIndexOf(","));
             myInnerArray.push(newFormula);
           } else {
             myInnerArray.push(col);
